8787952246bbcf5b0619699cf314b694298c10ff
ki-naba <inaba.swim@gmail.com>
2023-06-11 16:03:42 +0900

    1146. Snapshot Array

---
+++ problems/1146.snapshot-array.py
@@ -0,0 +1,32 @@
+#
+# @lc app=leetcode id=1146 lang=python3
+#
+# [1146] Snapshot Array
+#
+
+# @lc code=start
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.id = 0
+        self.history_records = [[[0, 0]] for _ in range(length)]
+        
+    def set(self, index: int, val: int) -> None:
+        self.history_records[index].append([self.id, val])
+
+    def snap(self) -> int:
+        self.id += 1
+        return self.id - 1
+
+    def get(self, index: int, snap_id: int) -> int:
+        snap_index = bisect.bisect_right(self.history_records[index], [snap_id, 10 ** 9])
+        return self.history_records[index][snap_index - 1][1]
+
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
+# @lc code=end
+
